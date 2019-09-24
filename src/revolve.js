c2cd6cc55a2c3fed9ffea5e41d24cbd10c36817d
hacksalot <hacksalot@indevious.com>
2019-09-23 23:12:08 -0400

    chore: bump version to 1.1.0

--- src/revolve.js
+++ src/revolve.js
@@ -1,7 +1,7 @@
 /**
 The Revolve.js library
 @file revolve.js
-@version 1.0.0
+@version 1.1.0
 @copyright Copyright (c) 2019 | James M. Devlin | https://revolvejs.org
 */
 //------------------------------------------------------------------------------
@@ -56,7 +56,7 @@ class RadialGauge
     // Set up default options
     let defOpts = {
       logicalSize: 512,
-      label: 'Revolve.js | v1.0.0',
+      label: 'Revolve.js | v1.1.0',
       mode: 'discrete',
       center: [0,0],
       radius: Math.min( ctx.canvas.width, ctx.canvas.height ) / 2.0
